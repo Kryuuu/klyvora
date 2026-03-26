d4fc6144184836da988b750210745302f84f49f6
Kryuu <Ahmadshaw257@gmail.com>
2026-03-26 21:41:14 +0800

    AAAAAAAAAAAAAAAAA

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,7 @@
 'use client'
 
 import Link from 'next/link'
-import { usePathname } from 'react-navigation/next' // Incorrect import in previous turns, but I will check for correct next/navigation
-import { usePathname as useNextPathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 
 const menuItems = [
   { name: 'Dashboard', href: '/dashboard', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
@@ -12,7 +11,7 @@ const menuItems = [
 ]
 
 export function Sidebar() {
-  const pathname = useNextPathname()
+  const pathname = usePathname()
 
   return (
     <aside className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r border-[#272733] bg-[#0f0f14]">
